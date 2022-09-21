049bd4a49d073054671886a2399bc1726ec15d58
Mia <mia.chokechaikul@jetstar.com>
2022-09-21 11:39:07 +1000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+export default {
+  buildOptions: {
+    // site: "https://www.henryjenkins.com.au",
+    sitemap: true // Generate sitemap (set to "false" to disable)
+  },
+  devOptions: {
+    port: 3003
+  }
+  // renderers: ["@astrojs/renderer-preact"]
+};
